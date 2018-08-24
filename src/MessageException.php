acade8730acf6139f453d404140c1c60b2f32a7f
xuteng <xut05@mingyuanyun.com>
2018-08-24 18:19:06 +0800

    优化

--- src/MessageException.php
+++ src/MessageException.php
@@ -6,11 +6,12 @@ namespace Xutengx\Exception;
 use Exception;
 
 /**
- * 此异常类抛出的异常, 将会被`ExceptionHandler`中间件捕获, 并进行友好的响应
+ * 此异常类抛出的异常, 可以将消息展示给用户, 进行友好的响应
+ * Class MessageException
+ * @package Xutengx\Exception
  */
 class MessageException extends Exception {
-	public function __construct($message = null, int $code = 500, $previous = null) {
-		$msg = $message ?? obj(Response::class)->getMessageByHttpCode($code);
-		parent::__construct($msg, $code, $previous);
+	public function __construct($message = '', int $code = 500, $previous = null) {
+		parent::__construct($message, $code, $previous);
 	}
 }
