95657c23c841d0605e724fa0f1d31533736b4d38
xuteng <xut05@mingyuanyun.com>
2018-08-27 11:53:29 +0800

    优化

--- src/HttpException.php
+++ src/HttpException.php
@@ -4,12 +4,10 @@ declare(strict_types = 1);
 namespace Xutengx\Exception;
 
 use Exception;
-use Xutengx\Core\Response;
 
 class HttpException extends Exception {
 	public function __construct($message = null, int $code = 400, $previous = null) {
-		$msg = $message ?? obj(Response::class)->getMessageByHttpCode($code);
-		parent::__construct($msg, $code, $previous);
+		parent::__construct($message, $code, $previous);
 	}
 
 }
